a447be262e8a5d5f40c7b4a28fed18085e1849cb
emilianogomar <emiliano.gomar07@gmail.com>
2024-02-18 16:21:19 -0300

    agrego funciones de monitoreo

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,9 +1,12 @@
 from typing import List, Tuple
 from datetime import datetime
 import pandas as pd
+import gc
 from pandas import json_normalize
+from memory_profiler import profile
 
-def q1_memory1(file_path: str) -> List[Tuple[datetime.date, str]]:
+@profile
+def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     
     # read Json File
     df = pd.read_json(file_path, lines=True)
@@ -14,8 +17,9 @@ def q1_memory1(file_path: str) -> List[Tuple[datetime.date, str]]:
     #Add json prefix to all json columns
     normalized_user.columns = [f'json_{col}' for col in normalized_user.columns]
 
-    # Combine normalized DataFrame with original DataFrame
+    # Combine normalized DataFrame with original DataFrame and delete the normalized DF
     df = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
+    del normalized_user
 
     # Drop all columns except username and date
     columnas_a_mantener = ['date', 'json_username']
@@ -25,6 +29,9 @@ def q1_memory1(file_path: str) -> List[Tuple[datetime.date, str]]:
     df['date'] = df['date'].dt.date
     df['json_username'] = df['json_username'].astype(str)
 
+    # Release non use memory
+    gc.collect()
+
     # Get top 10 dates with more tweets
     top_dates = df['date'].value_counts().head(10)
 
@@ -32,6 +39,37 @@ def q1_memory1(file_path: str) -> List[Tuple[datetime.date, str]]:
     users_top_dates = {}
     for fecha in top_dates.index:
         users_top_dates[fecha] = df[df['date'] == fecha]['json_username'].value_counts().idxmax()
+
+    del df
+    gc.collect()
+
+    # Convert to proper output format
+    date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
+
+    return date_list
+
+@profile
+def q1_memory_unoptimized(file_path: str) -> List[Tuple[datetime.date, str]]:
+
+    # read Json File
+    df = pd.read_json(file_path, lines=True)
+
+    # Normalize user field
+    normalized_user = json_normalize(df['user'])
+
+    #Add json prefix to all json columns
+    normalized_user.columns = [f'json_{col}' for col in normalized_user.columns]
+
+    # Combine normalized DataFrame with original DataFrame
+    df_2 = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
+
+    # Get top 10 dates with more tweets
+    top_dates = df_2['date'].value_counts().head(10)
+
+    # Get users with more tweets for top 10 dates 
+    users_top_dates = {}
+    for fecha in top_dates.index:
+        users_top_dates[fecha] = df_2[df_2['date'] == fecha]['json_username'].value_counts().idxmax()
 
     # Convert to proper output format
     date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,33 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
+from pandas import json_normalize
+from memory_profiler import profile
 
+@profile
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+
+    # read Json File
+    df = pd.read_json(file_path, lines=True)
+
+    # Normalize user field
+    normalized_user = json_normalize(df['user'])
+
+    #Add json prefix to all json columns
+    normalized_user.columns = [f'json_{col}' for col in normalized_user.columns]
+
+    # Combine normalized DataFrame with original DataFrame
+    df_2 = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
+
+    # Get top 10 dates with more tweets
+    top_dates = df_2['date'].value_counts().head(10)
+
+    # Get users with more tweets for top 10 dates 
+    users_top_dates = {}
+    for fecha in top_dates.index:
+        users_top_dates[fecha] = df_2[df_2['date'] == fecha]['json_username'].value_counts().idxmax()
+
+    # Convert to proper output format
+    date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
+
+    return date_list
