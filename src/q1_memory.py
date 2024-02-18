7511f10a80e53bb731900dcdcaa7eda3e4f4cfbb
emilianogomar <emiliano.gomar07@gmail.com>
2024-02-18 20:42:16 -0300

    Agrego funciones del q2

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -22,8 +22,8 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     del normalized_user
 
     # Drop all columns except username and date
-    columnas_a_mantener = ['date', 'json_username']
-    df = df.drop(columns=[col for col in df.columns if col not in columnas_a_mantener])
+    columns_to_keep = ['date', 'json_username']
+    df = df.drop(columns=[col for col in df.columns if col not in columns_to_keep])
 
     # Change columns types
     df['date'] = df['date'].dt.date
@@ -42,34 +42,6 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
 
     del df
     gc.collect()
-
-    # Convert to proper output format
-    date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
-
-    return date_list
-
-@profile
-def q1_memory_unoptimized(file_path: str) -> List[Tuple[datetime.date, str]]:
-
-    # read Json File
-    df = pd.read_json(file_path, lines=True)
-
-    # Normalize user field
-    normalized_user = json_normalize(df['user'])
-
-    #Add json prefix to all json columns
-    normalized_user.columns = [f'json_{col}' for col in normalized_user.columns]
-
-    # Combine normalized DataFrame with original DataFrame
-    df_2 = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
-
-    # Get top 10 dates with more tweets
-    top_dates = df_2['date'].value_counts().head(10)
-
-    # Get users with more tweets for top 10 dates 
-    users_top_dates = {}
-    for fecha in top_dates.index:
-        users_top_dates[fecha] = df_2[df_2['date'] == fecha]['json_username'].value_counts().idxmax()
 
     # Convert to proper output format
     date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,35 @@
 from typing import List, Tuple
+from datetime import datetime
+import pandas as pd
+import gc
+import regex
+from collections import Counter
+from pandas import json_normalize
+from memory_profiler import profile
 
+@profile
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+    # read Json File
+    df = pd.read_json(file_path, lines=True)
+
+    columns_to_keep = ['content']
+    df = df.drop(columns=[col for col in df.columns if col not in columns_to_keep])
+
+    # Counter of emojis
+    total_emojis = Counter()
+
+    # identify emojis in twitter content and count them
+    for tweet_text in df['content']:
+        emojis = regex.findall(r'[\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251\U0001f926-\U0001f937\U0001F1E0-\U0001F1FF]{1}', tweet_text)
+        total_emojis.update(Counter(emojis))
+
+    del df
+    gc.collect()
+
+    # Get top 10 emojis used
+    top_10_emojis = total_emojis.most_common(10)
+
+    emoji_list = [(emoji, count) for emoji, count in top_10_emojis]
+
+    return emoji_list
