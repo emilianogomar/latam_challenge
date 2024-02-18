467c4615cce44332e08bd53f7864c0800c8e8940
emilianogomar <emiliano.gomar07@gmail.com>
2024-02-18 18:37:46 -0300

    agrego funcion para q1 time

--- src/q1_time.py
+++ src/q1_time.py
@@ -7,27 +7,29 @@ from memory_profiler import profile
 @profile
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
 
-    # read Json File
     df = pd.read_json(file_path, lines=True)
 
-    # Normalize user field
+    # Normalize the DataFrame
     normalized_user = json_normalize(df['user'])
 
     #Add json prefix to all json columns
     normalized_user.columns = [f'json_{col}' for col in normalized_user.columns]
 
     # Combine normalized DataFrame with original DataFrame
-    df_2 = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
+    df = pd.concat([df.drop(columns='user'), normalized_user], axis=1)
 
-    # Get top 10 dates with more tweets
-    top_dates = df_2['date'].value_counts().head(10)
+    # Convertir la columna de fecha a tipo datetime si no está en ese formato
+    df['date'] = df['date'].dt.date
 
-    # Get users with more tweets for top 10 dates 
-    users_top_dates = {}
-    for fecha in top_dates.index:
-        users_top_dates[fecha] = df_2[df_2['date'] == fecha]['json_username'].value_counts().idxmax()
+    # Agrupar por fecha y usuario, y contar el número de tweets por usuario en cada día
+    user_tweets_per_day = df.groupby('date')['json_username'].agg(lambda x: x.value_counts().idxmax()).reset_index()
 
-    # Convert to proper output format
-    date_list = [(fecha, valor) for fecha, valor in users_top_dates.items()]
+    # Obtener el top 10 de días con más tweets
+    top_10_days = df.groupby('date').size().reset_index(name='count').nlargest(10,'count')
 
-    return date_list
+    # Para cada día en el top 10, obtener el usuario con más tweets
+    top_user_per_day = top_10_days.merge(user_tweets_per_day, on='date', how='left')
+
+    return_list = list(zip(top_user_per_day['date'], top_user_per_day['json_username']))
+
+    return return_list
