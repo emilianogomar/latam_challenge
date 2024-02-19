b9c91535ea1446a9272b53b9b0d7339e4bbec14a
emilianogomar <emiliano.gomar07@gmail.com>
2024-02-18 21:48:52 -0300

    agrego funciones para q3

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,27 @@
 from typing import List, Tuple
+from datetime import datetime
+import pandas as pd
+import gc
+from memory_profiler import profile
 
-def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+@profile
+def q3_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
+    
+    # Read the json file
+    df = pd.read_json(file_path, lines=True)
+
+    # Discard the rows with no mencioned users
+    df = df[df['mentionedUsers'].apply(lambda x: bool(x))]
+
+    # flatten column mencionedUsers
+    df = df.explode('mentionedUsers')
+
+    # count for each username the amount of apperances
+    username_counts = df['mentionedUsers'].apply(lambda x: x['username']).value_counts().head(10)
+
+    del df
+    gc.collect()
+
+    mencioned_list = [(value, count) for value, count in username_counts.items()]
+
+    return mencioned_list
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,24 @@
 from typing import List, Tuple
+from datetime import datetime
+import pandas as pd
+import gc
+from memory_profiler import profile
 
+@profile
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+    # Read the json file
+    df = pd.read_json(file_path, lines=True)
+
+    # Discard the rows with no mencioned users
+    df_filtered = df[df['mentionedUsers'].apply(lambda x: bool(x))]
+
+    # flatten column mencionedUsers
+    df_flattened = df_filtered.explode('mentionedUsers')
+
+    # count for each username the amount of apperances
+    username_counts = df_flattened['mentionedUsers'].apply(lambda x: x['username']).value_counts().head(10)
+
+    mencioned_list = [(value, count) for value, count in username_counts.items()]
+
+    return mencioned_list
